10dff4adf7bca3eb5ab72036b2261e86eae9cacd
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-05-19 21:38:27 +0300

    add baseclass

--- baseClass.py
+++ baseClass.py
@@ -11,3 +11,15 @@ import compare_pic
 from pathlib import Path
 import imutils
 import psycopg2
+
+class Human:
+    """This class ia a base human class"""
+    human_count = 0
+    age = 25
+
+    def __init__(self):
+        """Constructor"""
+        pass
+    def save_state(self):
+        #Save state to base
+        pass
