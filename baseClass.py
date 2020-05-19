df8639b2ab76ccc6428767dc4e871306acb3a8b7
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-05-19 22:27:00 +0300

    add main

--- baseClass.py
+++ baseClass.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import os
-import cv2
-import numpy as np
-import time
-import sys
-import traceback
-import datetime
-import compare_pic
-from pathlib import Path
-import imutils
-import psycopg2
 
 class Human:
     """This class ia a base human class"""
     human_count = 0
-    age = 25
+    age = 0
+    weight = 0
+    temperature = 36.6
+    state_of_health = 5
 
     def __init__(self):
         """Constructor"""
+        Human.human_count += 1
+        self.age = Human.age
+        self.weight = Human.weight
+        self.temperature = Human.temperature
+        self.state_of_health = Human.state_of_health
         pass
+
     def save_state(self):
         #Save state to base
         pass
+
+    def __str__(self):
+        return "Human class Object"
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import baseClass
+
+vasy = baseClass.Human()
