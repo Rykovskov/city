6a2bbdeb268abd7fbcda5ad5e511fbc92794912e
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-05-19 21:20:04 +0300

    add baseclass

---
+++ baseClass.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+import cv2
+import numpy as np
+import time
+import sys
+import traceback
+import datetime
+import compare_pic
+from pathlib import Path
+import imutils
+import psycopg2
